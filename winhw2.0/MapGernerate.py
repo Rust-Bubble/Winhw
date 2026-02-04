4d3e065e6af06d523d8298e2dbb9487a22b35740
Rust-Bubble <undertaker18025515285@outlook.com>
2026-02-04 17:47:14 +0800

    Add files via upload

--- winhw2.0/MapGernerate.py
+++ winhw2.0/MapGernerate.py
@@ -1,7 +1,8 @@
 import random
 import csv
 import os
-def Generatesrcmap(size,filepath):
+#生成地图点，size为点的数量
+def GeneratePointMap(size,filepath):
     res = []
     type = ['DORMITORY','DORMITORY','DORMITORY','TEACHING_BUILDING','TEACHING_BUILDING','TEACHING_BUILDING','EXIT']
     id = 1
@@ -23,59 +24,57 @@ def Generatesrcmap(size,filepath):
         element.append(e_position)
         element.append(e_isburning)
         res.append(element)
-    filepath += '/srcmap.csv'
     header = ['id','name','type','position','isburning']
     with open(filepath, mode='w', newline='', encoding='gb2312') as file:
         writer = csv.writer(file)  # 创建一个 writer 对象
+        writer.writerow([size,])
         writer.writerow(header)
         writer.writerows(res)  # 写入多行
 
-def GenerateArrs(Maxsize,filepath):
-    JamArr = []
-    TypeArr = []
-    SaveArr = []
-    DisArr = []
-    types = ["stair","corridor","road"]
-    for i in range(Maxsize):
-        for j in range(Maxsize):
-            is_used = random.choice([False,True])
-            if i > j :
-                SaveArr.append(SaveArr[j*Maxsize+j+i])
-                DisArr.append(DisArr[j*Maxsize+j+i])
-                JamArr.append(JamArr[j*Maxsize+j+i])
-                TypeArr.append(TypeArr[j*Maxsize+j+i])
-                continue
-            elif i == j or not is_used:
-                SaveArr.append("-1 ")
-                DisArr.append("-1 ")
-                JamArr.append("-1.0 ")
-                TypeArr.append("NONE ")
-                continue
-            SaveArr.append(str(random.randint(0,10))+' ')
-            TypeArr.append(random.choice(types)+' ')
-            DisArr.append(str(random.randint(20,100)+round(random.random(),2))+' ')
-            JamArr.append(str(round(random.uniform(0.0,1.0),1))+' ')
-        SaveArr.append('\n')
-        DisArr.append('\n')
-        JamArr.append('\n')
-        TypeArr.append('\n')
-    writetxt(filepath + "/TypeArr.txt",TypeArr)
-    writetxt(filepath + "/SaveArr.txt",SaveArr)
-    writetxt(filepath + "/JamArr.txt",JamArr)
-    writetxt(filepath + "/DisArr.txt",DisArr)
+#查重
+def is_existed(beg_id,end_id,testlist):
+    for sub_list in testlist:
+        if  (sub_list[0] == beg_id and sub_list[1] == end_id) or (sub_list[1] == beg_id and sub_list[0] == end_id):
+            return True
+    return False
+#生成地图边
+def GenerateEdges(Maxsize,filepath):
+    type_choices = ["stair","corridor","road"]
+    id_choices = [x for x in range(1,Maxsize+1)]
+    resault = []
+    header = ['begin_id','end_id','type','congestion','distance','riskLevel']
+    minEdge = Maxsize   #最小边数
+    maxEdge = Maxsize*(Maxsize-1)/2 #最大边数
+    EdgeNum = random.randint(minEdge,maxEdge)   #选择边数
+    for i in range(EdgeNum):
+        beg_id = random.choice(id_choices)
+        end_id = random.choice(id_choices)
+        #查重+避免自环
+        while beg_id == end_id or is_existed(beg_id,end_id,resault):
+            beg_id = random.choice(id_choices)
+            end_id = random.choice(id_choices)
+        type = random.choice(type_choices)
+        congestion = round(random.uniform(0.0,1.0),1)
+        distance = random.randint(20,100)+round(random.random(),2)
+        riskleval = random.randint(0,10)
+        edge = [beg_id,end_id,type,congestion,distance,riskleval]
+        resault.append(edge)
+    with open(filepath, mode='a', newline='', encoding='gb2312') as file:  
+        writer = csv.writer(file)  # 创建一个 writer 对象
+        writer.writerow([EdgeNum,])
+        writer.writerow(header)
+        writer.writerows(resault)  # 写入多行
 
-def writetxt(filepath,list_in):
-    with open(filepath,"w",encoding="gb2312") as file:
-        file.writelines(list_in)
 
 if __name__ == "__main__":
-    print("请输入地图个数：")
+    print("请输入地图数量：")
     num = eval(input())
     print("请输入地图点个数：")
     size = eval(input())
-    pos = "Maps"
+    pos = "./Maps"
+    os.makedirs(pos, exist_ok=True)  # 创建 Maps 目录
     for i in range(num):
-        Mappos = pos + '/map' + str(i)
-        os.makedirs(Mappos,exist_ok=True)
-        Generatesrcmap(size,Mappos)
-        GenerateArrs(size,Mappos)
+        filename = '/map' + str(i) + '.csv'
+        filepath = pos + filename  # 构造完整文件路径
+        GeneratePointMap(size, filepath)
+        GenerateEdges(size, filepath)
